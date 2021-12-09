ceb8590db0b278b39cb2917b72a7271e0de57ea3
AhmedMA0 <10253@iespuertasdelcampo.es>
2021-12-09 12:14:28 +0100

    dñflsflbhn

---
+++ JavaScript/mio/hola.js
@@ -0,0 +1,14 @@
+let punto = {x:5, y:6, mostrarCoordenadas:function(){
+    return `(${punto.x},${punto.y})`;
+    }
+}
+
+let punto = {
+    x:19,
+    y:36,
+    muestraXY:function(){
+        return x,y;
+    }
+}
+
+console.log(punto.mostrarXY);
