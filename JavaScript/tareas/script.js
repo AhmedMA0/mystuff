04e900ccf790c5ebca3e03eaf776f136a981855c
AhmedMA0 <10253@iespuertasdelcampo.es>
2021-12-13 14:08:06 +0100

    dñlnhdflsknbdaf

---
+++ JavaScript/tareas/script.js
@@ -0,0 +1,40 @@
+function ParImpar(num) {
+    
+    this.x=num;
+
+    this.error= function () {
+        if (isNaN(this.x)) {
+            console.error('Dato erroneo')
+        }
+        else console.log('Dato correcto.')
+    }
+
+    this.resuelve= function () {
+        this.x % 2 ? alert('Impar') : alert('Par');
+    }
+
+    this.es = (x) => x % 2 ? 'impar' : 'par';
+
+    this.muestraRandom = function () {
+
+        let rand = 0;
+        let cad = '';
+
+        for (let i = 0; i < 10; i++) {
+            rand = parseInt(Math.random() * 10000 + 1);
+            cad += rand + ' es ' + this.es(rand) + '. ';
+        }
+
+        document.writeln(cad);
+    }
+
+}
+
+ejemplo = new ParImpar('aaa');
+ejemplo.error();
+
+ejemplo = new ParImpar(50);
+ejemplo.error();
+ejemplo.resuelve();
+ejemplo.muestraRandom();
+
